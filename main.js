bc0226129e3b4db8f0ba6ca340e8c593a4ea53f2
Marcus Jackson <jacksonm2220@gmail.com>
2020-03-26 12:39:04 -0600

    fixed an arror

--- main.js
+++ main.js
@@ -7,18 +7,23 @@ function watchForm(){
 }
 function getDogImage(){
     let userInput = $('#breed-input').val();
+   
     fetch(`https://dog.ceo/api/breed/${userInput}/images/random`)
         .then(response => response.json())
         .then(responseJson => displayResults(responseJson))
         .catch(error => alert('Something went wrong. Try again later.'))
-};
-function displayResults(responseJson){
-     console.log(responseJson);
-     $('.results-img').replaceWith(`<img src="${responseJson.message}" class="results-img">`)
     
-    $('.dog-pictures').removeClass('hidden'); 
 };
 
+function displayResults(responseJson){
+     console.log(responseJson);
+     if(responseJson.status == "error"){
+         alert('cant find breed');
+     }else{
+     $('.results-img').replaceWith(`<img src="${responseJson.message}" class="results-img">`);
+     $('.dog-pictures').removeClass('hidden');
+     };
+};
 
 $(function(){
     console.log('App ready to run! Waiting for submit!');
