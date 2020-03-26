5e0f5fcb03cd9d80652cae8115123528ea06b45e
Marcus Jackson <jacksonm2220@gmail.com>
2020-03-25 20:26:22 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+'use strict';
+function watchForm(){
+    $('form').submit(event => {
+        event.preventDefault();
+        getDogImage();
+    });  
+}
+function getDogImage(){
+    let userInput = $('#breed-input').val();
+    fetch(`https://dog.ceo/api/breed/${userInput}/images/random`)
+        .then(response => response.json())
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => alert('Something went wrong. Try again later.'))
+};
+function displayResults(responseJson){
+     console.log(responseJson);
+     $('.results-img').replaceWith(`<img src="${responseJson.message}" class="results-img">`)
+    
+    $('.dog-pictures').removeClass('hidden'); 
+};
+
+
+$(function(){
+    console.log('App ready to run! Waiting for submit!');
+    watchForm();
+});
